cf5e25bfa7ec06ed19fef0c92a13cc2d816d83df
fujusong <fujusong@users.noreply.github.com>
2020-05-26 11:37:12 +0800

    update the config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,26 +1,50 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
-import babel from 'rollup-plugin-babel'
+import json from '@rollup/plugin-json'
+import babel from '@rollup/plugin-babel'
+import pkg from './package.json'
+import {terser} from 'rollup-plugin-terser'
 
-const pkg = require('./package.json')
 const libraryName = pkg.name
+const filePath = 'dist/'+pkg.name+'.';
+
+console.log(pkg.name)
 
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true, minify: true },
-    { file: pkg.module, format: 'es', sourcemap: true, minify: true }
+    { 
+      file: pkg.main, 
+      name: camelCase(libraryName), 
+      format: 'umd', 
+      sourcemap: true
+    },
+    { 
+      file: filePath+'umd.min.js',
+      format: 'umd', 
+      sourcemap: true,
+      name: 'version',
+      plugins: [terser()]
+    },
+    { 
+      file: pkg.module, 
+      format: 'es', 
+      sourcemap: true
+    },
+    {
+      file: pkg.cjsmodule,
+      format: `cjs`
+    }
+    
   ],
   watch: {
     include: 'src/**',
   },
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
-  global: {},
   plugins: [
     // Allow json resolution
     json(),
